f7715545305cf2ece7417af5e4ad34be923dcfbb
kunkurdonov <shirab.k@indriver.com>
2022-12-26 22:27:29 +0600

    next

--- src/NewFirstTest.java
+++ src/NewFirstTest.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -72,6 +74,16 @@ public class NewFirstTest {
         WebElement element = waitForElementPresent(by,error_message, timeoutSeconds);
         element.clear();
         return element;
+    }
+
+    protected void swipeUp (int timeOfSwipe) {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width/2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+
+        action.press(x,start_y).waitAction(timeOfSwipe).moveTo(x,end_y).release().perform();
     }
 
     @Test
@@ -214,5 +226,38 @@ public class NewFirstTest {
                 "Cannot find 'X' to cancel button",
                 5
         );
+    }
+
+    @Test
+    public void testSwipeArticle () {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia input'",
+                5);
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' topic searching by 'Java'",
+                5
+        );
+
+        WebElement title_element = waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find page title",
+                20
+        );
+
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+
     }
 }
