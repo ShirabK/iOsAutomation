17e4f65a7418442f5c6a2a811b29f72102c9f8d7
kunkurdonov <shirab.k@indriver.com>
2022-12-27 23:56:15 +0600

    next

--- src/Metods.java
+++ src/Metods.java
@@ -54,6 +54,53 @@ public class Metods {
         int start_y = (int) (size.height * 0.8);
         int end_y = (int) (size.height * 0.2);
 
-        action.press(x,start_y).waitAction(timeOfSwipe).moveTo(x,end_y).release().perform();
+        action.press(x,start_y)
+                .waitAction(timeOfSwipe)
+                .moveTo(x,end_y)
+                .release()
+                .perform();
+    }
+
+    public void swipeUpQuick () {
+        swipeUp(200);
+    }
+
+    public void swipeUpToFindElement (By by, String error_message, int max_swipes) {
+        int already_swipe = 0;
+/*        driver.findElements(by);
+        driver.findElements(by).size();*/
+
+        while (driver.findElements(by).size() == 0) {
+
+            if (already_swipe > max_swipes) {
+                waitForElementPresent(by, "\nCannot find element by swipe up.\n" + error_message, 0);
+                return;
+            }
+
+            swipeUpQuick();
+            ++already_swipe;
+        }
+    }
+
+    public void swipeElementOfLeft (By by, String error_message) {
+        WebElement element = waitForElementPresent(
+                by,
+                error_message,
+                10
+        );
+
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y + lower_y) / 2;
+
+        TouchAction action = new TouchAction(driver);
+
+        action.press(right_x,middle_y)
+                .waitAction(150)
+                .moveTo(left_x,middle_y)
+                .release()
+                .perform();
     }
 }
--- src/NewFirstTest.java
+++ src/NewFirstTest.java
@@ -142,7 +142,7 @@ public class NewFirstTest extends Metods{
                 20
         );
 
-        String article_title = title_element.getAttribute("text");
+        String article_title = title_element.getText();
 
         Assert.assertEquals (
                 "We see unexpected title",
@@ -209,5 +209,145 @@ public class NewFirstTest extends Metods{
         swipeUp(2000);
         swipeUp(2000);
 
+    }
+
+    @Test
+    public void testSwipeADownToFooter () {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia input'",
+                5);
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Appium",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Automation for Apps']"),
+                "Cannot find 'Automation for Apps' topic searching by 'Appium'",
+                5
+        );
+
+        WebElement title_element = waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find page title",
+                20
+        );
+
+        swipeUpToFindElement(
+                By.id("org.wikipedia:id/page_external_link"),
+                "Cannot find  the end of the article",
+                5
+        );
+    }
+    @Test
+    public void saveFirstArticleToMyListTitle () {
+
+        String name_of_folder = "Java";
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia input'",
+                5);
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' topic searching by 'Java'",
+                5
+        );
+
+        WebElement title_element = waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find page title",
+                20
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@content-desc='More options']"),
+                "Cannot find button to open article options",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find button to add article to reading options",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find button to 'Got it' tip overlay",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of article folder",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot clear input element to set name of article folder",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot put text into article folder input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("android:id/button1"),
+                "Cannot find or press 'OK' button to create folder",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@content-desc='Navigate up']"),
+                "Cannot find close article, cannot find 'x' click",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find 'My lists' button and click",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + name_of_folder + "']"),
+                "Cannot find created folder list article",
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find added article 'Java (programming language)'",
+                5
+        );
+
+        swipeElementOfLeft(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find saved article"
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot delete saved article",
+                5
+        );
     }
 }
